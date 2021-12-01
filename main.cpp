cb7fcbe83e2c1340cf59aec55ec6c97aea97b835
CabaleiroNicolas <nicocabaleiro2002@gmail.com>
2021-11-30 22:18:32 -0300

    Creacion de Primera Estructura del programa.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+
+int MenuRecepcion()
+{
+
+}
+
+int MenuAdministracion()
+{
+
+}
+
+int MenuEspacios()
+{
+
+}
+
+main()
+{
+    int opcion = 0;
+
+    do
+    {
+
+        system("cls");
+        opcion = ..........();
+        system("cls");
+
+       switch(opcion)
+       {
+           case 1:
+                {
+
+
+                     break;
+                }
+            case 2:
+                {
+
+
+                     break;
+                }
+            case 3:
+                {
+
+
+                     break;
+                }
+            case 5:
+                {
+
+
+                     break;
+                }
+
+       }
+
+    } while (opcion != 0);
+    
+
+
+}

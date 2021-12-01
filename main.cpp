3160776fb71d9b45b70b4506f8f1628765c7726d
CabaleiroNicolas <nicocabaleiro2002@gmail.com>
2021-12-01 02:01:22 -0300

    Implementacion de Menus.

--- main.cpp
+++ main.cpp
@@ -2,20 +2,81 @@
 #include <stdio.h>
 #include <string.h>
 
+int MenuGeneral()
+{
+    int opc = 0;
+
+    printf("\n\n1.- Modulo Espacios.");
+    printf("\n2.- Modulo Recepcionista");
+    printf("\n3.- Modulo Administracion");
+    printf("\n0.- Cerrar Aplicacion");
+    printf("\n\nIngrese una Opcion: ");
+    scanf("%d", &opc);
+
+    return opc;
+}
+
+
 
 int MenuRecepcion()
 {
 
+    int opc = 0;
+
+    printf("\n     MODULO RECEPCION");
+    printf("\n===============================");
+
+    printf("\n\n1.- Iniciar Sesion");
+    printf("\n2.- Registrar Cliente");
+    printf("\n3.- Registrar Turno");
+    printf("\n4.- Listado de Atenciones por Profesional y Fecha");
+    printf("\n0.- Cerrar Aplicacion");
+
+    printf("\n\nIngrese una Opcion: ");
+    scanf("%d", &opc);
+
+    return opc;
 }
+
+
 
 int MenuAdministracion()
 {
+     int opc = 0;
 
+    printf("\n     MODULO ADMINISTRACION");
+    printf("\n===============================");
+
+    printf("\n\n1.- Registrar Profesional");
+    printf("\n2.- Registrar Usuario Recepcionista");
+    printf("\n3.- Atenciones por Profesional");
+    printf("\n4.- Ranking de Profesionales por Atenciones");
+    printf("\n0.- Cerrar Aplicacion");
+
+    printf("\n\nIngrese una Opcion: ");
+    scanf("%d", &opc);
+
+    return opc;
 }
+
+
 
 int MenuEspacios()
 {
+    int opc = 0;
 
+    printf("\n     MODULO ESPACIOS");
+    printf("\n===============================");
+
+    printf("\n\n1.- Iniciar Sesion");
+    printf("\n2.- Visualizar Lista de Espera de Turnos (informe)");
+    printf("\n3.- Registrar Evolucion del tratamiento");
+    printf("\n0.- Cerrar Aplicacion");
+
+    printf("\n\nIngrese una Opcion: ");
+    scanf("%d", &opc);
+
+    return opc;
 }
 
 main()
@@ -26,7 +87,7 @@ main()
     {
 
         system("cls");
-        opcion = ..........();
+        opcion = MenuGeneral();
         system("cls");
 
        switch(opcion)
@@ -49,11 +110,19 @@ main()
 
                      break;
                 }
-            case 5:
+            case 4:
                 {
 
 
                      break;
+                }
+            default :
+                {
+                    printf("\n=======================================");
+                    printf("\nEsa Opcion no se encuentra en el Menu.\n\n");
+                    printf("\n=======================================");
+                    system("pause");
+                    break;
                 }
 
        }
